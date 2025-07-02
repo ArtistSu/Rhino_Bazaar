5cee3236d30036707b32fa94b4b9e254c787caab
ArtistS-win <sylgg0918@gmail.com>
2025-07-02 23:12:33 +0100

    init &set remote mysql

---
+++ Rhino_Bazaar/src/main/java/org/artists/rhino_bazaar/RhinoBazaarApplication.java
@@ -0,0 +1,13 @@
+package org.artists.rhino_bazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhinoBazaarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhinoBazaarApplication.class, args);
+    }
+
+}
---
+++ Rhino_Bazaar/src/test/java/org/artists/rhino_bazaar/RhinoBazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.artists.rhino_bazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhinoBazaarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
